a35eafc7b871a79bf245616a7951c907be7e9610
firatstory.eth <talefirat@gmail.com>
2022-10-06 17:14:02 +0200

    create getWeth

---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,10 @@
+async function main() {
+    // the protocol treats everything as an ERC@) token
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
---
+++ scripts/getWeth.js
@@ -0,0 +1,18 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+const AMOUNT = ethers.utils.parseEther("0.02")
+
+async function getWeth() {
+    const { deployer } = await getNamedAccounts()
+    // Weth Gorli 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6
+    const iWeth = await ethers.getContractAt(
+        "IWeth",
+        "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
+        deployer
+    )
+    const tx = await iWeth.deposit({ value: AMOUNT })
+    tx.wait(1)
+    const wethBalance = await iWeth.balanceOf(deployer)
+    console.log("wethBalance :>> ", wethBalance)
+}
+
+module.exports = { getWeth }
