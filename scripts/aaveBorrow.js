58b342a533a6276a75f27f77d98f4f9c7f31d56d
firatstory.eth <talefirat@gmail.com>
2022-10-07 10:16:44 +0200

    add addresses

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -5,6 +5,7 @@ async function main() {
     // the protocol treats everything as an ERC@) token
     await getWeth()
     const { deployer } = await getNamedAccounts()
+    // Aave Lending Pool Address Provider = 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
 }
 
 main()
--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -4,6 +4,7 @@ const AMOUNT = ethers.utils.parseEther("0.02")
 async function getWeth() {
     const { deployer } = await getNamedAccounts()
     // Weth Gorli 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6
+    // Weth Mainnet 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
     const iWeth = await ethers.getContractAt(
         "IWeth",
         "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
