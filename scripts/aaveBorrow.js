bc74a25d014bf9ad76d61929b31377cb097fabc0
firatstory.eth <talefirat@gmail.com>
2022-10-07 15:38:53 +0200

    deposit weth to aave

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,5 +1,5 @@
 const { getNamedAccounts, ethers } = require("hardhat")
-const { getWeth } = require("./getWeth")
+const { getWeth, amount } = require("./getWeth")
 
 async function main() {
     // the protocol treats everything as an ERC@) token
@@ -9,6 +9,14 @@ async function main() {
     console.log(`LendingPoll address ${lendingPool.address}`)
 
     // deposit
+    const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    // approve
+    await approveErc20(wethTokenAddress, lendingPool.address, amount, deployer)
+    console.log("Depositing...")
+    await lendingPool.deposit(wethTokenAddress, amount, deployer, 0)
+    console.log("Deposited!")
+
+    // Borrow
 }
 
 async function getLendingPool(account) {
@@ -22,6 +30,14 @@ async function getLendingPool(account) {
     const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
     return lendingPool
 }
+
+async function approveErc20(erc20Address, spenderAddress, amountToSpend, account) {
+    const erc20Token = await ethers.getContractAt("IERC20", erc20Address, account)
+    const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+    await tx.wait(1)
+    console.log("Approved!")
+}
+
 main()
     .then(() => process.exit(0))
     .catch((error) => {
